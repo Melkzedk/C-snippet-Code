b1beb3938ff4e36fd20c1d7ef228c56e4d3f1d44
Melkzedk <melkywafula29@gmail.com>
2025-09-24 16:08:34 +0300

    changes

---
+++ C Sharp Project/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Project
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            String name = "John Doe";
+            int age = 20;
+            Console.WriteLine(name + age);
+            Console.ReadLine();
+        }
+    }
+}
