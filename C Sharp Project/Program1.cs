16c13ea7b9e37c4aaa99db2297e24cd91c6a7ee3
melkzedek wafula <melkywafula29@gmail.comm>
2025-09-24 15:58:01 +0300

    first commit

---
+++ C Sharp Project/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Project
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "12345";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+                Console.ReadLine();
+            }
+            catch (Exception)
+           {
+                Console.WriteLine("The number couldn't be converted to byte");
+                Console.ReadLine();
+            }
+        }
+    }
+}
