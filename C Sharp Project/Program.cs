6982d7cd508778564c7cfd4b7aa5a3d427549635
Melkzedk <melkywafula29@gmail.com>
2025-10-06 21:05:29 +0300

    chnges

--- C Sharp Project/Program.cs
+++ C Sharp Project/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Project
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            int day = 12;
+            switch (day)
+            {
+                case 10:
+                    Console.WriteLine("Monday");
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Error!!");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
