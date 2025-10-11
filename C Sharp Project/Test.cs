e4bbc45225c50ffc9b184ae4995a095af4b629ab
melkzedek wafula <melkywafula29@gmail.comm>
2025-10-11 07:45:59 +0300

    changes

--- C Sharp Project/Test.cs
+++ C Sharp Project/Test.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Project
+{
+    class Test
+    {
+        static void Main4(string[] args)
+        {
+            String name = "John Doe";
+            int age = 20;
+            int marks = 60;
+            Console.WriteLine(name + " is "   +age);
+
+            if (marks > 40) {
+                Console.WriteLine("Passesd!!");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Failed!!");
+                Console.ReadLine();
+                break;
+            }
+        }
+    }
+}
