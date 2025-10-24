656d7d66b0f0b0884961299f2c97ac1cb51f36c3
Melkzedk <melkywafula29@gmail.com>
2025-10-24 09:40:01 +0300

    small changes

--- C Sharp Project/Program.cs
+++ C Sharp Project/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// A simple C# program demonstrating the use of a switch statement
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Project
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            int day = 12;
+            switch (day)
+            {
+                case 10:
+                    Console.WriteLine("Monday");
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 8:
+                    Console.WriteLine("Funday");
+                    break;
+                default:
+                    Console.WriteLine("Error! The day is not valid.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
